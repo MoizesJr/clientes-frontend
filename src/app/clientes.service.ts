b2b753f0b39414579690152778ec188255644081
Moizés Júnior <moizes.tj@gmail.com>
2026-01-27 21:15:48 -0300

    feat: Criação de component clientes-lista, listando cliente api(java) e navegação entre componentes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Cliente } from './clientes/clientes-form/cliente';
+import { Cliente } from './clientes/cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +16,8 @@ export class ClientesService {
     return this.http.post('http://localhost:8080/clientes', cliente);
   }
 
-  getClientes() : Cliente{
-    let cliente : Cliente = new Cliente();
-    cliente.id = 1;
-    cliente.nome = "Cliente Exemplo";
-    cliente.cpf = "88.785.52301-01";
-    return cliente;
+  buscarTodos() : Observable<Cliente[]> { 
+  return this.http.get<Cliente[]>('http://localhost:8080/clientes');
   }
+
 }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Cliente } from './cliente';
+import { Cliente } from '../cliente';
 import { ClientesService } from 'src/app/clientes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clientes-form',
@@ -13,7 +14,9 @@ export class ClientesFormComponent implements OnInit {
   success: boolean = false;
   errors: String[];
 
-  constructor( private service: ClientesService) {
+  constructor( 
+    private router: Router,
+    private service: ClientesService) {
     this.cliente = new Cliente();
    }
 
@@ -34,4 +37,9 @@ export class ClientesFormComponent implements OnInit {
       }
     );
   }
+
+  voltarParaLista() {
+    this.router.navigate(['/clientes-lista']);
+  }
+
 }
